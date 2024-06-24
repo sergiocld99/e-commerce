911aab244a4382ecb33509d981815190fddb83fd
Sergio Calderón <bs.sergiocalderon@gmail.com>
2024-06-24 19:12:59 -0300

    Componente Buscador (Search) genérico

---
+++ src/Search.jsx
@@ -0,0 +1,49 @@
+import { View, Text, TextInput, StyleSheet, Pressable } from 'react-native'
+import React, { useState } from 'react'
+
+// icons
+import { AntDesign } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+
+// Componente "Buscador"
+// Recibe la función a llamar (con argumento input) cuando se presiona la acción de Buscar
+const Search = ({onSearch}) => {
+  // estado del input
+  const [input, setInput] = useState("")
+
+  // acciones
+  const performSearch = () => onSearch(input)
+  const clearInput = () => setInput("")
+  
+  return (
+    <View style={styles.container}>
+      <TextInput style={styles.input} value={input} 
+        onChangeText={setInput} placeholder='Search product...'
+      />
+      <Pressable onPress={performSearch} >
+        <AntDesign name="search1" size={24} color="black" />
+      </Pressable>
+      <Pressable onPress={clearInput} >
+        <Entypo name="cross" size={24} color="black" />
+      </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 2,
+    borderColor: 'green',
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    flexDirection: 'row',
+    gap: 10
+  },
+
+  input: {
+    width: 200
+  }
+})
+
+export default Search
